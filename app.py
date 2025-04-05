6d094da9c10f0500e6bfe65d91ddfc78aaa8c9df
Tyler Rorah <tylerrorah@gmail.com>
2025-04-04 20:53:02 -0400

    defined some routes, created a tasks json file, added some templates for landing pages(a.i. generated) idk html

---
+++ app.py
@@ -0,0 +1,36 @@
+import json
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+os.path.exists("tasks.json")
+
+with open("tasks.json", "r") as file:
+    tasks_file = json.load(file)
+
+def add_tasks(tasks_file):
+    #this should be triggered off of a button click on the website
+    #then they should be prompted to fill out the fields the json file expects
+    #after the data has been validated we will write to the json file
+    #return the user back to the view tasks page
+    #this will not all be one function, just putting words on paper lol
+    pass
+    
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/tasks", methods=["GET"])
+def tasks():
+    return render_template("tasks.html")
+
+# @app.route.post("/add")
+
+# @app.route.put("/update/<id>")
+
+# @app.route.delete("/delete/<id>")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
